c66bb397bcdbe1252c0db09b7cfe6d553a7a1619
Christopher Everett <cce29555@gmail.com>
2023-04-30 12:23:16 -0400

    Accidently made it so everyone was blocked except whoever was whitelisted, this has been fixed, not blue checks are gone unless whitelisted

--- content.js
+++ content.js
@@ -1,35 +1,33 @@
 const checkmarkSelector = 'svg[data-testid="icon-verified"]';
+
 let whitelist = []; // Add user handles to the whitelist
 if (chrome.storage) {
   // Fetch the whitelist from storage and store it in memory
   chrome.storage.local.get('whitelist', (storageData) => {
-    
     whitelist = storageData.whitelist || [];
   });
 } else {
   console.error('chrome.storage is not available');
 }
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
- 
   if (request.type === 'request_data') {
-    // get the data you want to send to the popup
-    const data = whitelist
+    const data = whitelist;
     sendResponse({ data });
-   
   } else if (request.type === 'update_whitelist') {
-    // Update the whitelist in memory
     whitelist = request.whitelist;
-  
-    // Perform the required action with the updated whitelist and send a response, if needed
     sendResponse({ success: true });
   }
 });
 
+const shouldRemove = (container, username) => {
+  const checkmarkElement = container.querySelector(checkmarkSelector);
+  const hasCheckmark = checkmarkElement != null;
+  return (hasCheckmark && !whitelist.includes(username)) || (!hasCheckmark && whitelist.includes(username));
+};
 
 const removeBackgroundImage = (container, username) => {
-  console.log('Checking whitelist for', username);
-  if (!isUserWhitelisted(username)) {
-    console.log(username + "is in not the whitelist, blocked")
+  if (shouldRemove(container, username)) {
     const imageDiv = container.querySelector('div[data-testid="tweetPhoto"]');
     if (imageDiv) {
       imageDiv.replaceChildren();
@@ -38,68 +36,67 @@ const removeBackgroundImage = (container, username) => {
 };
 
 const removeUserAvatar = (container, username) => {
-  if (!isUserWhitelisted(username)) {
-  const userAvatarDivs = container.querySelectorAll('div[data-testid="Tweet-User-Avatar"]');
-  userAvatarDivs.forEach((avatarDiv) => {
-    avatarDiv.replaceChildren();
-  });
-}
+  if (shouldRemove(container, username)) {
+    const userAvatarDivs = container.querySelectorAll('div[data-testid="Tweet-User-Avatar"]');
+    userAvatarDivs.forEach((avatarDiv) => {
+      avatarDiv.replaceChildren();
+    });
+  }
 };
 
 const removeUserDescription = (container, username) => {
-  if (!isUserWhitelisted(username)) {
-  const userDescriptionDivs = container.querySelectorAll('div[data-testid="UserDescription"]');
-  userDescriptionDivs.forEach((descriptionDiv) => {
-    descriptionDiv.replaceChildren();
-  });
-}
+  if (shouldRemove(container, username)) {
+    const userDescriptionDivs = container.querySelectorAll('div[data-testid="UserDescription"]');
+    userDescriptionDivs.forEach((descriptionDiv) => {
+      descriptionDiv.replaceChildren();
+    });
+  }
 };
 
 const removeUserAvatarContainer = (container, username) => {
-  if (!isUserWhitelisted(username)) {
+  if (shouldRemove(container, username)) {
     const userAvatarContainerDivs = container.querySelectorAll('div[data-testid^="UserAvatar-Container-"]');
-  userAvatarContainerDivs.forEach((avatarContainerDiv) => {
-    avatarContainerDiv.replaceChildren();
-  });
-}
+    userAvatarContainerDivs.forEach((avatarContainerDiv) => {
+      avatarContainerDiv.replaceChildren();
+    });
+  }
 };
 
 const removeTweetText = (container, username) => {
-  if (!isUserWhitelisted(username)) {
+  if (shouldRemove(container, username)) {
     const tweetTextElements = container.querySelectorAll('[data-testid="tweetText"]');
-  tweetTextElements.forEach((element) => {
-    element.remove();
-  });
-}
+    tweetTextElements.forEach((element) => {
+      element.remove();
+    });
+  }
 };
 
 const removeUserNameChildren = (container, username) => {
-  if (!isUserWhitelisted(username)) {
+  if (shouldRemove(container, username)) {
     const userNameElements = container.querySelectorAll('[data-testid="UserName"]');
-  userNameElements.forEach((element) => {
-    element.replaceChildren();
-  });
-}
+    userNameElements.forEach((element) => {
+      element.replaceChildren();
+    });
+  }
 };
 
 const removeHeaderPhotoChildren = (container, username) => {
-  if (!isUserWhitelisted(username)) {
+  if (shouldRemove(container, username)) {
     const headerPhotoElements = container.querySelectorAll('a[href*="/header_photo"]');
-  headerPhotoElements.forEach((element) => {
-    element.replaceChildren();
-  });
-}
+    headerPhotoElements.forEach((element) => {
+      element.replaceChildren();
+    });
+  }
 };
 
 const removePhotoChildren = (container, username) => {
-  if (!isUserWhitelisted(username)) {
+  if (shouldRemove(container, username)) {
     const photoElements = container.querySelectorAll('a[href*="/photo"]');
-  photoElements.forEach((element) => {
-    element.replaceChildren();
-  });
-}
+    photoElements.forEach((element) => {
+      element.replaceChildren();
+    });
+  }
 };
-
 
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
